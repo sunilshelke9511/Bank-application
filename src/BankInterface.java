7a3a792504a5ad08514297d7c89bc47a175e6d1c
unknown <you@example.com>
2023-02-10 10:27:29 +0530

    bank application

---
+++ src/BankInterface.java
@@ -0,0 +1,8 @@
+public interface BankInterface
+{
+
+    int getBalance();
+    String depositMoney(int amount);
+    String withdraw(int amount, String password);
+    double calculateInterest(int time);
+}
